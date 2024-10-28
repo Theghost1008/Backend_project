4fd46307e36e726bab547b7778c781c420364c58
Theghost1008 <virajchronos@gmail.com>
2024-10-28 12:22:33 +0530

    Utils code

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("Error", (err)=>{
+        console.log("Error: ", err);
+        throw err;
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port:${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed!!", err);
+    
+})
 
 
 
